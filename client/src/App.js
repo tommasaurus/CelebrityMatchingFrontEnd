c328461ec4fb7bdcc466c533c06e1a2ddcd64915
Tommy Qu <tommyqu03@gmail.com>
2024-10-05 18:11:50 -0400

    added google analytics to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Hero from "./component/hero/Hero";
 import Contact from "./component/contact/Contact";
@@ -6,14 +6,30 @@ import InfiniteScrollImages from "./component/scroll/Scroll";
 import { Navbar, Footer } from "./component/navbar/NavbarFooter";
 import TOS from "./component/TermsOfService/TOS";
 import PP from "./component/PrivacyPolicy/PP";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const [currentPage, setCurrentPage] = useState("home");
 
+  useEffect(() => {
+    // Google Analytics Initialization
+    if (window.gtag) {
+      window.gtag('js', new Date());
+      window.gtag('config', 'G-HHN4DE3TPL');
+    }
+  }, []);
+
   const navigateTo = (page) => {
     setCurrentPage(page);
     window.scrollTo(0, 0);
+
+    // Google Analytics page tracking
+    if (window.gtag) {
+      window.gtag('config', 'G-HHN4DE3TPL', {
+        page_path: `/${page}`,
+        page_title: page.charAt(0).toUpperCase() + page.slice(1), // Capitalize page title
+      });
+    }
   };
 
   const renderCurrentPage = () => {
